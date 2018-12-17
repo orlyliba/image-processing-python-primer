f438c19e07405d987cefbd4ac2ca8c8ff2c75f69
Orly Liba <orlyl@orlyl-macbookpro.roam.corp.google.com>
2018-12-16 22:00:21 -0800

    Added installer, updated photo

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PATH=$PATH:~/Library/Python/2.7/bin
+if [ $(which pip) -eq ""] ; then
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py --user
+fi
+if [ ! -f .venv ]; then
+    pip install --user virtualenv
+    virtualenv -p `which python2.7` .venv
+fi
+source .venv/bin/activate
+pip install -r requirments.txt
+jupyter notebook
